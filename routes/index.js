531faa1fbb81f67bd0cb002384a3e0decc5dbbca
PeterJausovec <peterj@microsoft.com>
2016-04-14 10:47:01 -0700

    Invoking the service.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
+'use strict';
+
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  var envVar = process.env.HELLOSERVICE_URL || '';
+
+  if (envVar) {
+    request(envVar + '/hello', function (err, resp, body) {
+      var response = '';
+      if (err) {
+        response = err;
+      } else {
+        response = body;
+      }
+
+      res.render('index', { helloServiceEnv: envVar, serviceResponse: response });
+    });
+  }
+
+  res.render('index', { helloServiceEnv: envVar, serviceResponse: '<URL not available>' });
+
 });
 
 module.exports = router;
